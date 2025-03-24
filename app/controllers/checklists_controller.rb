e74f460494f1bf1f583806c992d2bcae174972e9
Alexandr Seregin <a.seryogin@inventos.ru>
2025-03-24 21:51:57 +0300

    fix show checklists

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -1,10 +1,12 @@
 class ChecklistsController < ApplicationController
   def self.show_in_navbar? = true
 
-  def index; end
+  def index
+    @checklists = Checklist.where(id: current_user.projects.pluck(:id))
+  end
 
   def create
-    checklist = Checklist.new(params.permit(%i[head checklist expected_result test_module test_type]))
+    checklist = Checklist.new(params.permit(%i[head checklist expected_result test_module test_type project_id]))
     return unless checklist.save
 
     render json: { checklist: }
