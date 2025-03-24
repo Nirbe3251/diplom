76be7986849d708872ab4fa577e9e4fd141a59a3
Alexandr Seregin <a.seryogin@inventos.ru>
2025-03-24 21:44:08 +0300

    fix add checklist

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -2,4 +2,11 @@ class ChecklistsController < ApplicationController
   def self.show_in_navbar? = true
 
   def index; end
+
+  def create
+    checklist = CheckList.new(params.permit(%i[head checklist expected_result test_module test_type]))
+    return unless checklist.save
+
+    render json: { checklist: }
+  end
 end
--- app/models/test_case.rb
+++ app/models/test_case.rb
@@ -1,5 +1,4 @@
 class TestCase < ApplicationRecord
   enum priority: { low: 0, normal: 1, high: 2 }
   belongs_to :project
-  belongs_to :test_module
 end
