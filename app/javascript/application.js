bd13591b53d3b0642cc816141dfdf877432e217f
Alexandr Seregin <a.seryogin@inventos.ru>
2025-04-21 21:31:49 +0300

    add module test checks

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -17,42 +17,77 @@ Turbolinks.start()
 Rails.start()
 channels.createRoom()
 
+function handleChecklistInput(element) {
+    $(element).on('input', function (e) {
+        console.log(e.target.value);
+        $(element).html(e.target.value);
+        $('#save_form').trigger('change');
+    });
+};
+
 document.addEventListener('turbo:load', () => {
+
+    let currentLocation = window.location.pathname;
+    let parsedLocation = currentLocation.split('/').slice(1).join('_');
+
+    let formContext = localStorage.getItem(parsedLocation);
+
+    if (formContext !== null) {
+        console.log(formContext)
+        $('#save_form').replaceWith(formContext)
+    }
+
+    let checklistIdPrifix = 'checklist[checklists]'
+    $(`textarea[id^='${checklistIdPrifix}']`).each(function () {
+        handleChecklistInput($(this));
+    });
+
     $("#add_checklist_block").on('click', function () {
+
+
         let checklistBlock = $('.checklist_block');
 
-        let childrensChecklistBlock = checklistBlock.children()
-        let lastId = NaN
+        let childrensChecklistBlock = checklistBlock.children();
+        let lastId = NaN;
         if (childrensChecklistBlock.length == 1) {
-            lastId = 0
+            lastId = 0;
         } else {
-            let lastCheckName = childrensChecklistBlock[childrensChecklistBlock.length - 1].children[0].name
+            let lastCheckName = childrensChecklistBlock[childrensChecklistBlock.length - 1].children[0].name;
 
-            let splittedName = lastCheckName.split("[")
-            let elementsCount = splittedName.length
-            let lastCheck = splittedName[elementsCount - 1].replace(/\]/, '')
-            console.log(lastCheck)
+            let splittedName = lastCheckName.split("[");
+            let elementsCount = splittedName.length;
+            let lastCheck = splittedName[elementsCount - 1].replace(/\]/, '');
+            console.log(lastCheck);
 
-            lastId = Number(lastCheck)
-            lastId = isNaN(lastId) ? 0 : lastId
+            lastId = Number(lastCheck);
+            lastId = isNaN(lastId) ? 0 : lastId;
         }
 
+        console.log(lastId);
 
-        console.log(lastId)
-
-        let newId = lastId + 1
+        let newId = lastId + 1;
         let labelBlock = `<div class="col-sm-3"><p class="mb-0">${newId} проверка<i style="color: red">*</i></p></div>`;
+        let checklistName = `checklist[checklists][${newId}]`
         let checklistArea = `
             <div class="form-group col-sm-9">
                 <textarea
                     class="form-control form-control-user"
                     maxlength="5000" style="min-height: 3rem"
                     placeholder="Введите список проверок"
-                    name="checklist[checklists][${newId}]"
-                    id="checklist_checklist">
-                </textarea>
+                    name=${checklistName}
+                    id=${checklistName}></textarea>
             </div>
         `;
-        checklistBlock.append(labelBlock + checklistArea)
+        checklistBlock.append(labelBlock + checklistArea);
+        $('#save_form').trigger('change')
+
+        let checklistTextField = document.getElementById(checklistName)
+        handleChecklistInput(checklistTextField)
     });
-})
+
+    // save forms to local storage
+    $('#save_form').on('change', function () { localStorage.setItem(parsedLocation, $('#save_form').prop('outerHTML')); });
+
+    $('#save_form').on('submit', function () { localStorage.removeItem(parsedLocation); });
+});
+
