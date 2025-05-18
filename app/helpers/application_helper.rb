c127dcd830abd04744f682135faa6c9c71dad941
Alexandr Seregin <a.seryogin@inventos.ru>
2025-05-18 18:45:35 +0300

    fix sort navbar names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,7 @@ module ApplicationHelper
       path.gsub!(/\(\.:format\)/, '')
 
       { controller:, path:, localize_name: }
-    end.uniq.compact
+    end.uniq.compact.sort_by { |el| el[:localize_name].first }
   end
 
   def current_page
